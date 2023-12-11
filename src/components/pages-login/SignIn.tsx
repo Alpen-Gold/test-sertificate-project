e07da3f7187018e05bd03c8990832affb54acecf
AlpenGold0656 <alpengold150@gmail.com>
2023-12-11 22:33:29 +0500

    edit login/register

--- src/components/pages-login/SignIn.tsx
+++ src/components/pages-login/SignIn.tsx
@@ -1,111 +1,117 @@
-// import styled from "@emotion/styled";
-// import logoImg from "../../assets/img";
 import signUpImg from "../../assets/img/and-machines-vqTWfa4DjEk-unsplash 1.png";
-// import axios from "axios";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Box, TextField } from "@mui/material";
+import axios from "axios";
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 function SignIn() {
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
-  console.log(registerEmail, registerPassword);
-
-  // const [registerPasswordPrev, setRegisterPasswordPrev] = useState("");
 
   const location = useLocation();
   const navigate = useNavigate();
 
   console.log("Path:", location.pathname);
 
-  const ucerR = async () => {
-    navigate("/home");
+  const ucerR = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
 
-    // try {
-    //   const respons = await axios.post(
-    //     "http://192.168.1.48:8080/api/auth/register",
-    //     {
-    //       email: registerEmail,
-    //       password: registerPassword,
-    //       prePassword: registerPasswordPrev,
-    //     }
-    //   );
-    //   console.log(respons);
-    // } catch (error) {
-    //   alert(error);
-    // }
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/shop/users/login",
+        {
+          email: registerEmail,
+          password: registerPassword,
+        }
+      );
+
+      if (response.status === 200) {
+        toast.success("Good job 🥳");
+        setTimeout(() => {
+          navigate("/home");
+        }, 2000);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Sorry, write it right!");
+    }
   };
 
   return (
-    <Box
-      sx={{ display: { xs: "block", md: "flex" } }}
-      style={{ alignItems: "start", gap: "50px" }}
-    >
-      <Box sx={{ display: { xs: "none", md: "block" } }} className="wrapperImg">
-        <img
-          src={signUpImg}
-          alt="none"
-          style={{ minWidth: "100%", maxHeight: "100%", objectFit: "cover" }}
-        />
-      </Box>
-
+    <>
       <Box
-        sx={{ margin: { xs: "50px", md: "0 50px 0 0" } }}
-        className="wrapperForm"
+        sx={{ display: { xs: "block", md: "flex" } }}
+        style={{ alignItems: "start", gap: "50px" }}
       >
-        <h3 className="signin_text">Sign In Page</h3>
+        <Box
+          sx={{ display: { xs: "none", md: "block" } }}
+          className="wrapperImg"
+        >
+          <img
+            src={signUpImg}
+            alt="none"
+            style={{ minWidth: "100%", maxHeight: "100%", objectFit: "cover" }}
+          />
+        </Box>
 
-        <Box sx={{ mt: "30px" }} className="wrapper_btn">
-          <button className="google">
-            <img
-              src="https://www.gstatic.com/images/branding/product/2x/googleg_24dp.png"
-              alt=""
-            />
-            Continue With Google
-          </button>
+        <Box
+          sx={{ margin: { xs: "50px", md: "0 50px 0 0" } }}
+          className="wrapperForm"
+        >
+          <h3 className="signin_text">Sign In Page</h3>
 
-          <form style={{ marginTop: "16px" }}>
-            <div className="login_input">
-              <label htmlFor="email">User name or email address</label>
-              <TextField
-                error
-                id="email"
-                variant="outlined"
-                type="email"
-                placeholder="Enter your email..."
-                onChange={(e) => setRegisterEmail(e.target.value)}
-                helperText="Enter email!"
+          <Box sx={{ mt: "30px" }} className="wrapper_btn">
+            <button className="google">
+              <img
+                src="https://www.gstatic.com/images/branding/product/2x/googleg_24dp.png"
+                alt=""
               />
-            </div>
-
-            <div className="login_input">
-              <label htmlFor="password">Password</label>
-              <TextField
-                id="password"
-                variant="outlined"
-                type="password"
-                placeholder="Enter your password..."
-                onChange={(e) => setRegisterPassword(e.target.value)}
-              />
-              <a href="#" className="forgetRassword">
-                Forget your password
-              </a>
-            </div>
-
-            <button className="all-button " onClick={ucerR}>
-              Sign In
+              Continue With Google
             </button>
 
-            <Box sx={{ mt: "10px" }}>
-              <a href="#" className="forgetRassword">
-                Don’t have an account? Sign up
-              </a>
-            </Box>
-          </form>
+            <form style={{ marginTop: "16px" }}>
+              <div className="login_input">
+                <label htmlFor="email">User name or email address</label>
+                <TextField
+                  id="email"
+                  variant="outlined"
+                  type="email"
+                  placeholder="Enter your email..."
+                  onChange={(e) => setRegisterEmail(e.target.value)}
+                />
+              </div>
+
+              <div className="login_input">
+                <label htmlFor="password">Password</label>
+                <TextField
+                  id="password"
+                  variant="outlined"
+                  type="password"
+                  placeholder="Enter your password..."
+                  onChange={(e) => setRegisterPassword(e.target.value)}
+                />
+                <a href="#" className="forgetRassword">
+                  Forget your password
+                </a>
+              </div>
+
+              <button className="all-button " onClick={ucerR}>
+                Sign In
+              </button>
+
+              <Box sx={{ mt: "10px" }}>
+                <a href="#" className="forgetRassword">
+                  Don’t have an account? Sign up
+                </a>
+              </Box>
+            </form>
+          </Box>
         </Box>
       </Box>
-    </Box>
+      <ToastContainer />
+    </>
   );
 }
 
--- src/components/pages-login/SignUp.tsx
+++ src/components/pages-login/SignUp.tsx
@@ -1,119 +1,162 @@
-// import styled from "@emotion/styled";
-// import logoImg from "../../assets/img";
 import signUpImg from "../../assets/img/and-machines-vqTWfa4DjEk-unsplash 1.png";
-// import axios from "axios";
-
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
 import { Box, TextField } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 function SignUn() {
-  //   const [registerEmail, setRegisterEmail] = useState("");
-  //   const [registerPassword, setRegisterPassword] = useState("");
-  //   const [registerPasswordPrev, setRegisterPasswordPrev] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerPasswordPrev, setRegisterPasswordPrev] = useState("");
+  const [number, setNumber] = useState("");
 
   const location = useLocation();
   const navigate = useNavigate();
 
   console.log("Path:", location.pathname);
 
-  const ucerR = async () => {
-    navigate("/home");
+  const ucerR = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
 
-    // try {
-    //   const respons = await axios.post(
-    //     "http://192.168.1.48:8080/api/auth/register",
-    //     {
-    //       email: registerEmail,
-    //       password: registerPassword,
-    //       prePassword: registerPasswordPrev,
-    //     }
-    //   );
-    //   console.log(respons);
-    // } catch (error) {
-    //   alert(error);
-    // }
+    console.log(registerEmail, registerPassword, registerPasswordPrev);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/shop/users/register",
+        {
+          name: userName,
+          email: registerEmail,
+          password: registerPassword,
+          phone: number,
+        }
+      );
+
+      if (
+        response.status === 200 &&
+        registerPassword === registerPasswordPrev
+      ) {
+        toast.success("Good job 🥳");
+        setTimeout(() => {
+          navigate("/home");
+        }, 2000);
+      } else {
+        toast.error("Sorry, write it right!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <Box
-      sx={{ display: { xs: "block", md: "flex" } }}
-      style={{ alignItems: "start", gap: "50px" }}
-    >
-      <Box sx={{ display: { xs: "none", md: "block" } }} className="wrapperImg">
-        <img
-          src={signUpImg}
-          alt="none"
-          style={{ minWidth: "100%", maxHeight: "100%", objectFit: "cover" }}
-        />
-      </Box>
-
+    <>
       <Box
-        sx={{ margin: { xs: "50px", md: "0 50px 0 0" } }}
-        className="wrapperForm"
+        sx={{ display: { xs: "block", md: "flex" } }}
+        style={{ alignItems: "start", gap: "50px" }}
       >
-        <h3 className="signin_text">Sign Up</h3>
-
-        <p
-          style={{
-            color: "rgba(102, 102, 102, 0.80)",
-            textAlign: "start",
-          }}
+        <Box
+          sx={{ display: { xs: "none", md: "block" } }}
+          className="wrapperImg"
         >
-          Sign up for free to access to in any of our products
-        </p>
+          <img
+            src={signUpImg}
+            alt="none"
+            style={{ minWidth: "100%", maxHeight: "100%", objectFit: "cover" }}
+          />
+        </Box>
 
-        <div className="wrapper_btn">
-          <button className="google">
-            <img
-              src="https://www.gstatic.com/images/branding/product/2x/googleg_24dp.png"
-              alt=""
-            />
-            Continue With Google
-          </button>
+        <Box
+          sx={{ margin: { xs: "50px", md: "0 50px 0 0" } }}
+          className="wrapperForm"
+        >
+          <h3 className="signin_text">Sign Up</h3>
 
-          <form style={{ marginTop: "16px" }}>
-            <div className="login_input">
-              <label htmlFor="email">Email</label>
-              <TextField
-                error
-                id="email"
-                type="email"
-                variant="outlined"
-                placeholder="Enter your email..."
-                // onChange={(e) => setRegisterEmail(e.target.value)}
-                helperText="Enter email !"
+          <p
+            style={{
+              color: "rgba(102, 102, 102, 0.80)",
+              textAlign: "start",
+            }}
+          >
+            Sign up for free to access to in any of our products
+          </p>
+
+          <div className="wrapper_btn">
+            <button className="google">
+              <img
+                src="https://www.gstatic.com/images/branding/product/2x/googleg_24dp.png"
+                alt=""
               />
-            </div>
-
-            <div className="login_input">
-              <label htmlFor="password">Password</label>
-              <TextField
-                id="password"
-                type="password"
-                variant="outlined"
-                placeholder="Enter your password..."
-                // onChange={(e) => setRegisterPassword(e.target.value)}
-              />
-            </div>
-
-            <div className="login_input">
-              <label htmlFor="password Pre">Password Pre</label>
-              <TextField
-                id="password Pre"
-                type="password"
-                variant="outlined"
-                // onChange={(e) => setRegisterPasswordPrev(e.target.value)}
-                placeholder="Enter your password pre..."
-              />
-            </div>
-
-            <button className="all-button register_btn" onClick={ucerR}>
-              Registration
+              Continue With Google
             </button>
-          </form>
-        </div>
+
+            <form style={{ marginTop: "16px" }}>
+              <div className="login_input">
+                <label htmlFor="name">User</label>
+                <TextField
+                  id="name"
+                  type="text"
+                  variant="outlined"
+                  placeholder="Enter your User Name..."
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+              </div>
+
+              <div className="login_input">
+                <label htmlFor="email">Email</label>
+                <TextField
+                  id="email"
+                  type="email"
+                  variant="outlined"
+                  placeholder="Enter your email..."
+                  onChange={(e) => setRegisterEmail(e.target.value)}
+                />
+              </div>
+
+              <div className="login_input">
+                <label htmlFor="password">Password</label>
+                <TextField
+                  id="password"
+                  type="password"
+                  variant="outlined"
+                  placeholder="Enter your password..."
+                  onChange={(e) => setRegisterPassword(e.target.value)}
+                />
+              </div>
+
+              <div className="login_input">
+                <label htmlFor="password Pre">Password Pre</label>
+                <TextField
+                  id="password Pre"
+                  type="password"
+                  variant="outlined"
+                  onChange={(e) => setRegisterPasswordPrev(e.target.value)}
+                  placeholder="Enter your password pre..."
+                />
+              </div>
+
+              <div className="login_input">
+                <label htmlFor="Number Pre">Number Pre</label>
+                <TextField
+                  id="Number Pre"
+                  type="text"
+                  defaultValue="+998"
+                  variant="outlined"
+                  onChange={(e) => setNumber(e.target.value)}
+                  placeholder="Enter your password pre..."
+                />
+              </div>
+
+              <button className="all-button register_btn" onClick={ucerR}>
+                Registration
+              </button>
+            </form>
+          </div>
+        </Box>
       </Box>
-    </Box>
+      <ToastContainer />
+    </>
   );
 }
 
